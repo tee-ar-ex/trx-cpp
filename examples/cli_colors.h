52a54e6a5c2aeaa929e190f2019e73e97e6cd26c
mattcieslak <mattcieslak@gmail.com>
2026-01-28 15:03:02 -0500

    lint

--- examples/cli_colors.h
+++ examples/cli_colors.h
@@ -10,41 +10,35 @@
 #include <unistd.h>
 #endif
 
-namespace trx_cli
-{
-struct Colors
-{
-	bool enabled = false;
-	const char *reset = "\033[0m";
-	const char *bold = "\033[1m";
-	const char *cyan = "\033[36m";
-	const char *green = "\033[32m";
-	const char *yellow = "\033[33m";
-	const char *magenta = "\033[35m";
+namespace trx_cli {
+struct Colors {
+  bool enabled = false;
+  const char *reset = "\033[0m";
+  const char *bold = "\033[1m";
+  const char *cyan = "\033[36m";
+  const char *green = "\033[32m";
+  const char *yellow = "\033[33m";
+  const char *magenta = "\033[35m";
 };
 
-inline bool stdout_supports_color()
-{
+inline bool stdout_supports_color() {
 #if defined(_WIN32) || defined(_WIN64)
-	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (h == INVALID_HANDLE_VALUE || h == nullptr)
-	{
-		return false;
-	}
-	DWORD mode = 0;
-	return GetConsoleMode(h, &mode) != 0;
+  HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+  if (h == INVALID_HANDLE_VALUE || h == nullptr) {
+    return false;
+  }
+  DWORD mode = 0;
+  return GetConsoleMode(h, &mode) != 0;
 #else
-	return isatty(fileno(stdout)) != 0;
+  return isatty(fileno(stdout)) != 0;
 #endif
 }
 
-inline std::string colorize(const Colors &colors, const char *code, const std::string &text)
-{
-	if (!colors.enabled)
-	{
-		return text;
-	}
-	return std::string(code) + text + colors.reset;
+inline std::string colorize(const Colors &colors, const char *code, const std::string &text) {
+  if (!colors.enabled) {
+    return text;
+  }
+  return std::string(code) + text + colors.reset;
 }
 } // namespace trx_cli
 
