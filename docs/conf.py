abe6709fa2a2a59e0e6fa6be96b7fe5133ac747d
Ariel Rokem <arokem@gmail.com>
2026-08-06 09:33:13 -0700

    Transition to use sphinx with myst (#51)
    * Convert files to md for transition to uniform sphinx/myst look and feel.
    
    * Deploy docs with GHA instead of RTD.
    
    So we can be uniform across all projects, please.
    
    * Adds the GHA config.
    
    * Configure build more thoroughly.
    
    * Install more apt dependencies.
    
    * Upgrade this action.
    
    * Get rid of deploy-preview, which cannot be run on fork PRs.
    
    We'll need to download the artifact to preview.
    
    * Tweak documentation build for more uniformity with other languages.
    
    * Link to main site.
    
    So we always have a way to go back there.

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,7 @@ project = "trx-cpp"
 author = "trx-cpp contributors"
 
 extensions = [
+    "myst_parser",
     "breathe",
     "exhale",
     "sphinx.ext.autosectionlabel",
@@ -13,11 +14,44 @@ root_doc = "index"
 templates_path = ["_templates"]
 exclude_patterns = ["_build"]
 
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {
-    "collapse_navigation": False,
-    "navigation_depth": 3,
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
+
+html_theme = "pydata_sphinx_theme"
+html_logo = "_static/trx_logo.png"
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/tee-ar-ex/trx-cpp",
+            "icon": "fa-brands fa-github",
+        },
+    ],
+    "logo": {
+        "image_light": "_static/trx_logo.png",
+        "image_dark": "_static/trx_logo.png",
+        "alt_text": "TRX",
+        "link": "https://tee-ar-ex.github.io",
+    },
+    "show_toc_level": 2,
+    "navigation_depth": 4,
+    "navigation_with_keys": True,
+    "show_nav_level": 2,
+    "navbar_align": "left",
+    "header_links_before_dropdown": 5,
+}
+
+html_sidebars = {
+    "**": ["sidebar-nav-bs.html"],
+}
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "tasklist",
+]
 
 breathe_projects = {
     "trx-cpp": os.path.join(os.path.dirname(__file__), "_build", "doxygen", "xml"),
